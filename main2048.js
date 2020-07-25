a8ba978082e950252cf3ec76aa0ae6f2680c9de1
TYX <tm@TYXMBP.local>
2020-07-26 04:16:55 +0800

    modify main2048.js.

--- main2048.js
+++ main2048.js
@@ -195,7 +195,7 @@ document.addEventListener('touchend', function (event) {
     var deltax = endx - startx;
     var deltay = endy - starty;
 
-    if (Math.abs(deltax) < 0.3 * documentWidth && Math.abs(deltay) < 0.3 * documentWidth)
+    if (Math.abs(deltax) < 0.2 * documentWidth && Math.abs(deltay) < 0.2 * documentWidth)
         return;
 
     //x方向
